8ec547fe3d684e4fbf5b05afbdb5bd30ddb100ff
domonik <schdruzzi@gmail.com>
2023-10-24 15:57:06 +0200

    added Christmas Theme and fixed schedule

--- assets/js/restylePage.js
+++ assets/js/restylePage.js
@@ -3,15 +3,17 @@
             return Math.floor(Math.random() * (max - min + 1)) + min;
         }
 
-function displayRandomImage(svgImage, left, top, scale, rotation, zindex=-99) {
+function displayRandomImage(svgImage, left, top, scale, rotation, zindex=-99, image_id=null) {
     // Get a reference to the "img" folder
     var imgFolder = "assets/img/";
 
     // List of SVG image file names in the "img" folder
     // Choose a random SVG image from the list
     // Create an image element and set its attributes
-    var image = new Image();
-    image.src = imgFolder + svgImage;
+    var image = document.createElement("object");
+    image.id = image_id
+    image.data = imgFolder + svgImage; // Replace with your SVG file path
+    image.type = "image/svg+xml";
     image.style.width = "100px"
     image.style.transform = `rotate(${rotation}deg)`;
     image.style.transform += ` scale(${scale})`;
@@ -22,9 +24,9 @@ function displayRandomImage(svgImage, left, top, scale, rotation, zindex=-99) {
     image.style.top = top + "%"; // Adjust as needed
 
     // Add a click event listener to make the image disappear
-    image.addEventListener("click", function() {
-        document.body.removeChild(image);
-    });
+    //image.addEventListener("click", function() {
+    //    document.body.removeChild(image);
+    //});
 
     // Append the image to the body
     document.body.appendChild(image);
@@ -36,31 +38,82 @@ function determineTheme(){
   if (currentMonth == 9){
     halloweenTheme()
   }
+  if (currentMont == 11){
+    christmasTheme()
+  }
 
-}
-function changeFontFamily(fontFamily) {
-  const headerElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
-
-  headerElements.forEach(element => {
-    element.style.fontFamily = fontFamily;
-  });
 }
 
 function halloweenTheme(){
   displayRandomImage("bat.svg", 10, 10, 1.5, 25);
   displayRandomImage("bat.svg", 2, 30, 2, -30);
   displayRandomImage("bat.svg", 25, 60, 1, 10);
-  displayRandomImage("bat.svg", 7, 70, 0.5, -5);
-  displayRandomImage("bat.svg", 5, 73, 0.7, 8);
-  displayRandomImage("witch.svg", 90, 20, 1.5, 8);
+  displayRandomImage("witch.svg", 85, 20, 2, 8);
   displayRandomImage("pumpkin.svg", 90, 85, 3, 8, zindex=1);
   document.documentElement.style.setProperty('--ufr-main-blue', "#171512");
   document.documentElement.style.setProperty('--active-item-color', "rgb(219, 134, 7)");
   document.documentElement.style.setProperty('--selector-color', "rgba(219, 134, 7, 0.3)");
   document.documentElement.style.setProperty('--font-family', 'Creepster,"Source Sans Pro",Calibri,Candara,Arial,sans-serif');
+  document.body.style.backgroundImage = "url('assets/img/GraveYard.svg')";
+  document.body.style.backgroundSize = "cover"
+  document.body.style.backgroundAttachment = "fixed"
+  document.body.style.backgroundPosition = "center bottom"
+
+
+}
+
+
+
+function christmasTheme(){
+
+  document.documentElement.style.setProperty('--ufr-main-blue', "#007550");
+  document.documentElement.style.setProperty('--active-item-color', "#e30613ff");
+  document.documentElement.style.setProperty('--exercise-bg-color', "white");
+
+  document.body.style.backgroundColor = "#0F8A5F";
+  var script = document.createElement('script');
+
+  // Set the source attribute to the URL of snowflakes.js
+  script.src = 'assets/js/snowflakes.js';
+  script.onload = function() {
+    letItSnow();
+  };
+  document.head.appendChild(script);
+  document.body.style.backgroundImage = "url('assets/img/WinterWonderland.svg')";
+  document.body.style.backgroundSize = "cover"
+  document.body.style.backgroundAttachment = "fixed"
+  document.body.style.backgroundPosition = "center bottom"
+  displayRandomImage("reindeer.svg", 90, 85, 3, -8, zindex=1, image_id="reindeer-svg");
+  document.documentElement.style.setProperty('--font-family', 'Festive,"Source Sans Pro",Calibri,Candara,Arial,sans-serif');
+  var svgObject = document.getElementById("reindeer-svg");
+  var scrollTimeout;
+  svgObject.onload = function() {
+    var svgDocument = svgObject.contentDocument;
+    // Access and manipulate elements in the SVG
+    if (svgDocument) {
+        var svgElement = svgDocument.querySelector("svg");
+        const rect = svgElement.getElementById('reindeerNose');
+        document.addEventListener('scroll', function() {
+            rect.style.fill = "#e30613";
+            rect.style.stroke = "#e30613"
+            rect.style.strokeWidth = 1;
+            clearTimeout(scrollTimeout);
+            scrollTimeout = setTimeout(function() {
+              rect.style.fill = '#4e3824';
+              rect.style.stroke = "none"
+
+
+            }, 200);
+        });
+            // You can now access and manipulate elements in the SV
+    }
+};
+
+
 
 
 
 }
+
 
 window.onload = determineTheme()
